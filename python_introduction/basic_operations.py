fcc7a0ec5ac22a7a15df04d8b5329d4fdf0f9440
Scott Theuri <theuriscottorg@gmail.com>
2025-07-13 18:48:14 +0300

    Do basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5 
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print("Assuming number1 = 10 and number2 = 5")
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
