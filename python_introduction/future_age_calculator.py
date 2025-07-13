af148efa04e4302afc6ec73f717781be9d5c5c06
Scott Theuri <theuriscottorg@gmail.com>
2025-07-13 18:52:22 +0300

    Calculate future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# Program to calculate the future age of someone
+input_age  = int(input("How old are you?"))
+#Assuming the current year is 2023
+current_year = 2023
+years_to_add = 27
+future_age = input_age + years_to_add
+print("In 2050, you will be", future_age, "years old.")
