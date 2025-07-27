9ca3d064b1318c97bc216d3e8be6d0322e30fc13
Scott Theuri <theuriscottorg@gmail.com>
2025-07-27 23:04:27 +0300

    Initial script for ecplore_datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+import datetime
+from datetime import datetime, timedelta
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time: ", formatted_date)
+display_current_datetime() 
+
+def calculate_future_date():
+    current_date = datetime.now()
+    days_toadd = int(input("Enter number of days to add to the current date: "))
+    delta = timedelta(days=days_toadd)
+    future_date = current_date + delta
+    print("Future date: ", future_date.strftime("%Y-%m-%d"))
+calculate_future_date()
+
