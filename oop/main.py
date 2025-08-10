0918977e91cf6123198e39110b8714cc6c7b4adb
Scott Theuri <theuriscottorg@gmail.com>
2025-08-10 23:45:50 +0300

    Initil script for the class static methods demo.

--- oop/main.py
+++ oop/main.py
@@ -1,13 +1,11 @@
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
+from class_static_methods_demo import Calculator
 
 def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    sum_result = Calculator.add(10, 5) #Using the static method
+    print(f"The sum is: {sum_result}")
+
+    product_result = Calculator.multiply(10, 5) #Using class method
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
