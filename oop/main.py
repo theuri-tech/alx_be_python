ecf4b1d0e6b61922135be28de05b14a4ca710c29
Scott Theuri <theuriscottorg@gmail.com>
2025-08-10 23:32:46 +0300

    Initial script for the polyphormism demo

--- oop/main.py
+++ oop/main.py
@@ -1,20 +1,13 @@
-from library_system import Book, EBook, PrintBook, Library
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
 
 def main():
-    my_library = Library()   #Create a library instance
-
-    #Create instances of each book.
-    classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-    paper_novel = PrintBook("The Catcher in the Rye", "J.D Salinger", 234)
-
-    #add books to library
-    my_library.add_book(classic_book)
-    my_library.add_book(digital_novel)
-    my_library.add_book(paper_novel)
-
-    #list all books in the library
-    my_library.list_books()
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
 if __name__ == "__main__":
     main()
